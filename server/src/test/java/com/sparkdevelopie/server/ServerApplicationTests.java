16082972286cef816c24b6f35bcb36f5073d5566
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-01-28 15:56:06 +0530

    Initialize java spring backend

---
+++ server/src/test/java/com/sparkdevelopie/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkdevelopie.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
