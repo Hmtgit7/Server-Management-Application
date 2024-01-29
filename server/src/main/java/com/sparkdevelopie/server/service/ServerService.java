431af42ecc9739c6b46339dccefb9530548d8255
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-01-30 00:11:07 +0530

    server services

---
+++ server/src/main/java/com/sparkdevelopie/server/service/ServerService.java
@@ -0,0 +1,15 @@
+package com.sparkdevelopie.server.service;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import com.sparkdevelopie.server.Model.Server;
+
+public interface ServerService {
+    Server create(Server server);
+    Server ping(String ipAddress) throws IOException;
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+}
